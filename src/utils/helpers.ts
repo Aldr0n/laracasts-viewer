5c613ff52af07f91260f0e05e6c1580efdcf61c9
Ruben Maurer <aldr0n@protonmail.com>
2024-12-15 20:52:33 +0100

    feat(helpes): add debug & die helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,3 +4,8 @@ export const validateEmail = (email: string): boolean => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
 };
+
+export const dd = (string: string) => {
+    console.log(string);
+    process.exit(0);
+};
