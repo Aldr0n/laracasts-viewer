aa8590ab203e95506151f8cb3fc2ef1de15c4c66
Ruben Maurer <aldr0n@protonmail.com>
2024-12-16 02:43:08 +0100

    feat(core): add getPuppeteerPage util method

--- src/core/Page.ts
+++ src/core/Page.ts
@@ -33,4 +33,8 @@ export class Page implements IPageService {
             callback(page);
         });
     }
+
+    public async getPuppeteerPage(): Promise<PuppeteerPage> {
+        return await this.getPage();
+    }
 }
